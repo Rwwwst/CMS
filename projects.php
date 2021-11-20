e3c27d674ae5b99428ddf4253ee4a8c13fbc0eef
Rwwwst <94245983+Rwwwst@users.noreply.github.com>
2021-11-20 16:24:58 -0500

    Update projects.php

--- projects.php
+++ projects.php
@@ -38,9 +38,10 @@ include 'includes/wideimage/WideImage.php';
   <tr>
     <th></th>
     <th align="center">ID</th>
-    <th align="left">Title</th>
+    <th align="left">Property Name</th>
     <th align="center">Type</th>
-    <th align="center">Date</th>
+    <th align="center">From Date</th>
+    <th align="center">To Date</th>
     <th></th>
     <th></th>
     <th></th>
@@ -52,11 +53,12 @@ include 'includes/wideimage/WideImage.php';
       </td>
       <td align="center"><?php echo $record['id']; ?></td>
       <td align="left">
-        <?php echo htmlentities( $record['title'] ); ?>
+        <?php echo htmlentities( $record['propertyName'] ); ?>
         <small><?php echo $record['content']; ?></small>
       </td>
       <td align="center"><?php echo $record['type']; ?></td>
-      <td align="center" style="white-space: nowrap;"><?php echo htmlentities( $record['date'] ); ?></td>
+      <td align="center" style="white-space: nowrap;"><?php echo htmlentities( $record['fromDate'] ); ?></td>
+      <td align="center" style="white-space: nowrap;"><?php echo htmlentities( $record['toDate'] ); ?></td>
       <td align="center"><a href="projects_photo.php?id=<?php echo $record['id']; ?>">Photo</i></a></td>
       <td align="center"><a href="projects_edit.php?id=<?php echo $record['id']; ?>">Edit</i></a></td>
       <td align="center">
